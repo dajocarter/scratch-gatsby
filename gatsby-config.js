b24e1f339a91584966988c601416795b42731c85
David Carter <dajocarter@gmail.com>
2017-10-25 11:21:30 -0400

    Add WP source plugin to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `perfect.tribestaging.com`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: true,
+        verboseOutput: true
+      }
+    }
+  ]
+};
