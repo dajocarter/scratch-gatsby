56a896623b18d22924d810be01bb83ab2e3c4080
David Carter <dajocarter@gmail.com>
2017-10-25 21:46:49 -0400

    List all pages and posts on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,52 @@
 import React from "react";
+import Link from "gatsby-link";
 
-const IndexPage = () => (
+const IndexPage = props => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>All Pages</h1>
+    {props.data.allWordpressPage.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link to={node.slug}>
+          <h3>{node.title}</h3>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </div>
+    ))}
+    <h1>All Posts</h1>
+    {props.data.allWordpressPost.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link to={node.slug}>
+          <h3>{node.title}</h3>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </div>
+    ))}
   </div>
 );
 
 export default IndexPage;
+
+export const indexQuery = graphql`
+  query indexQuery {
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`;
