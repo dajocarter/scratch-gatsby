63ec784ba382b690cc0e20fdc94f7359bf793440
David Carter <dajocarter@gmail.com>
2017-10-25 21:36:20 -0400

    Create simple templates

---
+++ src/templates/page.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PageTemplate extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Page Template</h1>
+      </div>
+    );
+  }
+}
+
+export default PageTemplate;
---
+++ src/templates/single.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SingleTemplate extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Single Post Template</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleTemplate;
