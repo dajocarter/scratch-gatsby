174a5ceff7ed2df2dc7af2667fe37ffc9537c78b
David Carter <dajocarter@gmail.com>
2017-10-27 16:44:41 -0400

    Use archive template for Blog page

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
   return new Promise((resolve, reject) => {
     const postTemplate = path.resolve(`src/templates/single.js`);
     const pageTemplate = path.resolve(`src/templates/page.js`);
+    const archiveTemplate = path.resolve(`src/templates/archive.js`);
 
     resolve(
       graphql(`
@@ -38,13 +39,23 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
         // Create pages
         result.data.allWordpressPage.edges.forEach(({ node }) => {
           if (node.status === "publish") {
-            createPage({
-              path: `/${node.slug}/`,
-              component: pageTemplate,
-              context: {
-                id: node.wordpress_id
-              }
-            });
+            if (node.slug === "blog") {
+              createPage({
+                path: `/${node.slug}/`,
+                component: archiveTemplate,
+                context: {
+                  id: node.wordpress_id
+                }
+              });
+            } else {
+              createPage({
+                path: `/${node.slug}/`,
+                component: pageTemplate,
+                context: {
+                  id: node.wordpress_id
+                }
+              });
+            }
           }
         });
         // Create Posts
