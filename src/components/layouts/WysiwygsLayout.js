bbd3dab6fa02b71ee09fb019d1f677ae6b86e941
David Carter <dajocarter@gmail.com>
2017-10-28 00:12:22 -0400

    Complete WysiswygsLayout

--- src/components/layouts/WysiwygsLayout.js
+++ src/components/layouts/WysiwygsLayout.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
-
+import "./WysiwygsLayout.scss";
 class WysiwygsLayout extends Component {
+  renderHeader() {
+    return this.props.layout.header ? (
+      <h2 className="layout-header">{this.props.layout.header}</h2>
+    ) : (
+      ""
+    );
+  }
+
+  renderWysiwygs() {
+    let containerClass = "flex-container";
+    switch (this.props.layout.offset) {
+      case "2 to 1":
+        containerClass += " thirds-1-2";
+        break;
+
+      case "1 to 2":
+        containerClass += " thirds-2-1";
+        break;
+    }
+    return (
+      <div className={containerClass}>
+        {this.props.layout.wysiwygs.map((column, index) => {
+          return (
+            <div
+              className="flex-column"
+              key={index}
+              dangerouslySetInnerHTML={{ __html: column.wysiwyg }}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+
   render() {
     const layout = this.props.layout;
     return (
-      <div>
-        <h1>Wysiwyg</h1>
-      </div>
+      <section className="wysiwygs">
+        <div className="wrap">
+          {this.renderHeader()}
+          {this.renderWysiwygs()}
+        </div>
+      </section>
     );
   }
 }
