5630c3a439fc9a5127fa7ca1febbf19efd99c73a
David Carter <dajocarter@gmail.com>
2017-10-27 23:24:46 -0400

    Check for properties before adding to class

--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -1,9 +1,10 @@
 export function postClasses(post) {
-  let classes = `post-${post.wordpress_id} ${post.type} type-${post.type} status-${post.status} format-${post.format}`;
-
-  if (post.categories) {
+  let classes = `post-${post.wordpress_id}`;
+  if (post.type) classes += ` ${post.type} type-${post.type}`;
+  if (post.status) classes += ` status-${post.status}`;
+  if (post.format) classes += ` format-${post.format}`;
+  if (post.categories)
     post.categories.map(cat => (classes += ` category-${cat.slug}`));
-  }
 
   return classes;
 }
