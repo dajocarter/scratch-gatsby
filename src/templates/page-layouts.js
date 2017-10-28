3f9f25f90040d984fe056629ccfe7d2eccde734f
David Carter <dajocarter@gmail.com>
2017-10-28 05:40:40 -0400

    Query for added layouts and reorder

--- src/templates/page-layouts.js
+++ src/templates/page-layouts.js
@@ -4,8 +4,11 @@ import CardsLayout from "../components/layouts/CardsLayout";
 import ColumnsLayout from "../components/layouts/ColumnsLayout";
 import HeroLayout from "../components/layouts/HeroLayout";
 import ImageBlocksLayout from "../components/layouts/ImageBlocksLayout";
+import ImagesWithTextLayout from "../components/layouts/ImagesWithTextLayout";
+import LogosLayout from "../components/layouts/LogosLayout";
 import SliderLayout from "../components/layouts/SliderLayout";
 import StaggeredRowsLayout from "../components/layouts/StaggeredRowsLayout";
+import TogglesLayout from "../components/layouts/TogglesLayout";
 import VideoLayout from "../components/layouts/VideoLayout";
 import WysiwygsLayout from "../components/layouts/WysiwygsLayout";
 
@@ -14,38 +17,36 @@ const PageLayoutsTemplate = props => (
     <h1 dangerouslySetInnerHTML={{ __html: props.data.wordpressPage.title }} />
     {props.data.wordpressPage.acf.layout_page.map((layout, index) => {
       switch (layout.__typename) {
-        case "WordPressAcf_slider":
-          return <SliderLayout layout={layout} key={index} />;
-          break;
-
-        case "WordPressAcf_flexible_columns":
-          return <ColumnsLayout layout={layout} key={index} />;
-          break;
-
-        case "WordPressAcf_staggered_images_with_text":
-          return <StaggeredRowsLayout layout={layout} key={index} />;
-          break;
-
-        case "WordPressAcf_hero_unit":
-          return <HeroLayout layout={layout} key={index} />;
-          break;
-
-        case "WordPressAcf_wysiwygs":
-          return <WysiwygsLayout layout={layout} key={index} />;
-          break;
-
-        case "WordPressAcf_image_blocks":
-          return <ImageBlocksLayout layout={layout} key={index} />;
-          break;
-
-        case "WordPressAcf_video":
-          return <VideoLayout layout={layout} key={index} />;
-          break;
-
         case "WordPressAcf_cards":
           return <CardsLayout layout={layout} key={index} />;
           break;
-
+        case "WordPressAcf_hero_unit":
+          return <HeroLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_flexible_columns":
+          return <ColumnsLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_image_blocks":
+          return <ImageBlocksLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_logos_section":
+          return <LogosLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_multiple_images_with_text":
+          return <ImagesWithTextLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_slider":
+          return <SliderLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_staggered_images_with_text":
+          return <StaggeredRowsLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_video":
+          return <VideoLayout layout={layout} key={index} />;
+          break;
+        case "WordPressAcf_wysiwygs":
+          return <WysiwygsLayout layout={layout} key={index} />;
+          break;
         default:
           return;
           break;
@@ -68,17 +69,51 @@ export const pageQuery = graphql`
       acf {
         layout_page {
           __typename
-          ... on WordPressAcf_slider {
-            slides {
-              background
+          ... on WordPressAcf_cards {
+            number_of_columns
+            wordpress_2_col_offset
+            wordpress_3_col_offset
+            flexible_columns
+            content_choice
+            featured_posts {
+              wordpress_id
+              post_author
+              post_date
+              post_date_gmt
+              post_content
+              post_title
+              post_excerpt
+              post_status
+              post_name
+              post_modified
+              post_modified_gmt
+              post_parent
+              menu_order
+              post_type
+            }
+            cards {
+              image
               header
               blurb
               add_button
               internal_link
-              button_internal_link
               button_external_link
               button_text
             }
+          }
+          ... on WordPressAcf_hero_unit {
+            background_image
+            bg_image_pos_x
+            bg_image_pos_y
+            text_align
+            text_margin
+            header
+            blurb
+            add_button
+            internal_link
+            button_internal_link
+            button_external_link
+            button_text
           }
           ... on WordPressAcf_flexible_columns {
             number_of_columns
@@ -91,6 +126,57 @@ export const pageQuery = graphql`
               icon_or_image
               icon
               image
+              header
+              blurb
+              add_button
+              internal_link
+              button_internal_link
+              button_external_link
+              button_text
+            }
+          }
+          ... on WordPressAcf_image_blocks {
+            content_choice
+            offset
+            post_animate
+            post_animation
+            use_featured_posts: featured_posts
+            image_blocks {
+              background_image
+              header
+              blurb
+              animate
+              animation
+              add_button
+              internal_link
+              button_internal_link
+              button_external_link
+              button_text
+            }
+          }
+          ... on WordPressAcf_logos_section {
+            header
+            logo_display
+            logos {
+              logo
+              link
+            }
+          }
+          ... on WordPressAcf_multiple_images_with_text {
+            image_side
+            images {
+              url
+            }
+            header
+            content
+            add_button
+            internal_link
+            button_external_link
+            button_text
+          }
+          ... on WordPressAcf_slider {
+            slides {
+              background
               header
               blurb
               add_button
@@ -113,44 +199,12 @@ export const pageQuery = graphql`
               button_text
             }
           }
-          ... on WordPressAcf_hero_unit {
-            background_image
-            bg_image_pos_x
-            bg_image_pos_y
-            text_align
-            text_margin
+          ... on WordPressAcf_toggles {
             header
             blurb
-            add_button
-            internal_link
-            button_internal_link
-            button_external_link
-            button_text
-          }
-          ... on WordPressAcf_wysiwygs {
-            header
-            offset
-            wysiwygs {
-              wysiwyg
-            }
-          }
-          ... on WordPressAcf_image_blocks {
-            content_choice
-            offset
-            post_animate
-            post_animation
-            use_featured_posts: featured_posts
-            image_blocks {
-              background_image
-              header
-              blurb
-              animate
-              animation
-              add_button
-              internal_link
-              button_internal_link
-              button_external_link
-              button_text
+            toggles {
+              toggle_header
+              toggle_content
             }
           }
           ... on WordPressAcf_video {
@@ -166,39 +220,12 @@ export const pageQuery = graphql`
             button_link
             button_text
           }
-          ... on WordPressAcf_cards {
-            number_of_columns
-            wordpress_2_col_offset
-            wordpress_3_col_offset
-            flexible_columns
-            content_choice
-            featured_posts {
-              wordpress_id
-              post_author
-              post_date
-              post_date_gmt
-              post_content
-              post_title
-              post_excerpt
-              post_status
-              comment_status
-              ping_status
-              post_password
-              post_name
-              to_ping
-              pinged
-              post_modified
-              post_modified_gmt
-              post_content_filtered
-              post_parent
-              guid
-              menu_order
-              post_type
-              post_mime_type
-              comment_count
-              filter
+          ... on WordPressAcf_wysiwygs {
+            header
+            offset
+            wysiwygs {
+              wysiwyg
             }
-            cards
           }
         }
       }
