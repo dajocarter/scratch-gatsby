7699e9000a5fefdb3a0e171fa2e4158c78d176ee
David Carter <dajocarter@gmail.com>
2017-11-07 21:38:37 -0500

    Create slider with react-slick

--- src/components/layouts/SliderLayout.js
+++ src/components/layouts/SliderLayout.js
@@ -1,14 +1,55 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
+import FaChevronLeft from "react-icons/lib/fa/chevron-left";
+import FaChevronRight from "react-icons/lib/fa/chevron-right";
+import Slider from "react-slick";
+import "../Slick.scss";
 class SliderLayout extends Component {
   render() {
-    const layout = this.props.layout;
+    const settings = {
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      arrows: true,
+      prevArrow: (
+        <button type="button">
+          <FaChevronLeft aria-hidden="true" />
+          <span className="sr-only">Previous</span>
+        </button>
+      ),
+      nextArrow: (
+        <button type="button">
+          <span className="sr-only">Next</span>
+          <FaChevronRight aria-hidden="true" />
+        </button>
+      )
+    };
+
     return (
-      <div>
-        <h1>Slider</h1>
-      </div>
+      <Slider {...settings}>
+        {this.props.layout.slides.map((slide, index) => (
+          <div
+            key={`slide-${index}`}
+            style={{
+              backgroundImage: `url(${slide.background.source_url})`
+            }}
+          >
+            <div className="overlay" />
+            <div className="wrapper">
+              <div className="slide-text">
+                <h2 className="layout-header slide-header">{slide.header}</h2>
+                <div dangerouslySetInnerHTML={{ __html: slide.blurb }} />
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
     );
   }
 }
+
+SliderLayout.propTypes = {
+  layout: PropTypes.object.isRequired
+};
 
 export default SliderLayout;
