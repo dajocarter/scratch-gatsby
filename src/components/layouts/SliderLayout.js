76c196a3c6b4acc1e9b61da9ec83ca069abaad6a
David Carter <dajocarter@gmail.com>
2018-01-10 12:11:23 -0500

    Update Slider layout

--- src/components/layouts/SliderLayout.js
+++ src/components/layouts/SliderLayout.js
@@ -5,51 +5,53 @@ import FaChevronRight from "react-icons/lib/fa/chevron-right";
 import Slider from "react-slick";
 import "../Slick.scss";
 class SliderLayout extends Component {
-  render() {
-    const settings = {
-      autoplay: true,
-      autoplaySpeed: 3000,
-      dots: true,
-      arrows: true,
-      prevArrow: (
-        <button type="button">
-          <FaChevronLeft aria-hidden="true" />
-          <span className="sr-only">Previous</span>
-        </button>
-      ),
-      nextArrow: (
-        <button type="button">
-          <span className="sr-only">Next</span>
-          <FaChevronRight aria-hidden="true" />
-        </button>
-      )
-    };
+	render() {
+		const settings = {
+			autoplay: true,
+			autoplaySpeed: 3000,
+			dots: true,
+			arrows: true,
+			prevArrow: (
+				<button type="button">
+					<FaChevronLeft aria-hidden="true" />
+					<span className="sr-only">Previous</span>
+				</button>
+			),
+			nextArrow: (
+				<button type="button">
+					<span className="sr-only">Next</span>
+					<FaChevronRight aria-hidden="true" />
+				</button>
+			)
+		};
 
-    return (
-      <Slider {...settings}>
-        {this.props.layout.slides.map((slide, index) => (
-          <div
-            key={`slide-${index}`}
-            style={{
-              backgroundImage: `url(${slide.background.source_url})`
-            }}
-          >
-            <div className="overlay" />
-            <div className="wrapper">
-              <div className="slide-text">
-                <h2 className="layout-header slide-header">{slide.header}</h2>
-                <div dangerouslySetInnerHTML={{ __html: slide.blurb }} />
-              </div>
-            </div>
-          </div>
-        ))}
-      </Slider>
-    );
-  }
+		return (
+			<Slider {...settings}>
+				{this.props.layout.slides.map((slide, index) => (
+					<div
+						key={`slide-${index}`}
+						style={{
+							backgroundImage: `url(${
+								slide.background.localFile.childImageSharp.original.src
+							})`
+						}}
+					>
+						<div className="overlay" />
+						<div className="wrapper">
+							<div className="slide-text">
+								<h2 className="layout-header slide-header">{slide.header}</h2>
+								<div dangerouslySetInnerHTML={{ __html: slide.blurb }} />
+							</div>
+						</div>
+					</div>
+				))}
+			</Slider>
+		);
+	}
 }
 
 SliderLayout.propTypes = {
-  layout: PropTypes.object.isRequired
+	layout: PropTypes.object.isRequired
 };
 
 export default SliderLayout;
