f1857d49c909ffed255d9fbf7e838f97cf3a973b
David Carter <dajocarter@gmail.com>
2018-01-11 17:57:46 -0500

    Update Logos layout

--- src/components/LogoDisplay.js
+++ src/components/LogoDisplay.js
@@ -3,22 +3,25 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 
 const LogoDisplay = props => (
-  <div className="logo-display all-logos">
-    {props.logos.map(
-      (image, index) =>
-        image.link ? (
-          <Link key={`logo-${index}`} to={image.link}>
-            <img src={image.logo.source_url} />
-          </Link>
-        ) : (
-          <img key={`logo-${index}`} src={image.logo.source_url} />
-        )
-    )}
-  </div>
+	<div className="logo-display all-logos">
+		{props.logos.map(
+			(image, index) =>
+				image.link ? (
+					<Link key={`logo-${index}`} to={image.link.url}>
+						<img src={image.logo.localFile.childImageSharp.resolutions.src} />
+					</Link>
+				) : (
+					<img
+						key={`logo-${index}`}
+						src={image.logo.localFile.childImageSharp.resolutions.src}
+					/>
+				)
+		)}
+	</div>
 );
 
 LogoDisplay.propTypes = {
-  logos: PropTypes.arrayOf(PropTypes.object)
+	logos: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default LogoDisplay;
--- src/components/LogoSlick.js
+++ src/components/LogoSlick.js
@@ -4,57 +4,59 @@ import Slider from "react-slick";
 import "./Slick.scss";
 
 class LogoSlick extends Component {
-  render() {
-    const settings = {
-      autoplay: true,
-      dots: false,
-      arrows: false,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
+	render() {
+		const settings = {
+			autoplay: true,
+			dots: false,
+			arrows: false,
+			speed: 300,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3,
+						infinite: true,
+						dots: true
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
+		};
 
-    return (
-      <Slider className="logo-display slide-logos" {...settings}>
-        {this.props.logos.map((image, index) => (
-          <div key={`logo-${index}`} className="logo-img">
-            {image.link ? (
-              <Link to={image.link}>
-                <img src={image.logo.source_url} />
-              </Link>
-            ) : (
-              <img src={image.logo.source_url} />
-            )}
-          </div>
-        ))}
-      </Slider>
-    );
-  }
+		return (
+			<Slider className="logo-display slide-logos" {...settings}>
+				{this.props.logos.map((image, index) => (
+					<div key={`logo-${index}`} className="logo-img">
+						{image.link ? (
+							<Link to={image.link.url}>
+								<img
+									src={image.logo.localFile.childImageSharp.resolutions.src}
+								/>
+							</Link>
+						) : (
+							<img src={image.logo.localFile.childImageSharp.resolutions.src} />
+						)}
+					</div>
+				))}
+			</Slider>
+		);
+	}
 }
 
 // LogoSlick.propTypes = {
