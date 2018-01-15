f5dbfe622a83e56b6b28b85011bc44fd9c45934e
David Carter <dajocarter@gmail.com>
2018-01-15 01:07:16 -0500

    Convert toggles to styled-components

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,48 +1,83 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import FaChevronDown from "react-icons/lib/fa/chevron-down";
 import FaChevronUp from "react-icons/lib/fa/chevron-up";
 import { VelocityTransitionGroup } from "velocity-react";
 import "./Accordion.scss";
 
+const Toggle = styled.div``;
+
+const ToggleTitle = styled.h2`
+	border: 1px solid #3d9970;
+	color: #3d9970;
+	cursor: pointer;
+	display: block;
+	margin: 1.5rem auto 0;
+	padding: 0.5rem 1.75rem 0.5rem 0.5rem;
+	position: relative;
+
+	.toggle-icon {
+		bottom: 10px;
+		color: #3d9970;
+		display: block;
+		line-height: inherit;
+		position: absolute;
+		right: 0.5rem;
+	}
+`;
+
+const ToggleContent = styled.div`
+	padding: 1rem 1rem 0;
+	position: relative;
+	transition: all 0.2s ease;
+
+	p {
+		&:first-child {
+			margin-top: 0;
+		}
+
+		&:last-child {
+			margin-bottom: 0;
+		}
+	}
+`;
+
 class Accordion extends Component {
-  constructor() {
-    super();
-    this.state = {
-      showContent: false
-    };
-  }
-  handleClick() {
-    this.setState({
-      showContent: !this.state.showContent
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h2 className="toggle-heading" onClick={this.handleClick.bind(this)}>
-          {this.props.header}
-          {this.state.showContent ? (
-            <FaChevronUp className="toggle-icon" />
-          ) : (
-            <FaChevronDown className="toggle-icon" />
-          )}
-        </h2>
-        <VelocityTransitionGroup
-          enter={{ animation: "slideDown", duration: 200 }}
-          leave={{ animation: "slideUp", duration: 200 }}
-        >
-          {this.state.showContent ? (
-            <div
-              className="toggle-content"
-              dangerouslySetInnerHTML={{ __html: this.props.content }}
-            />
-          ) : (
-            undefined
-          )}
-        </VelocityTransitionGroup>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			showContent: false
+		};
+	}
+	handleClick() {
+		this.setState({
+			showContent: !this.state.showContent
+		});
+	}
+	render() {
+		return (
+			<Toggle>
+				<ToggleTitle onClick={this.handleClick.bind(this)}>
+					{this.props.header}
+					{this.state.showContent ? (
+						<FaChevronUp className="toggle-icon" />
+					) : (
+						<FaChevronDown className="toggle-icon" />
+					)}
+				</ToggleTitle>
+				<VelocityTransitionGroup
+					enter={{ animation: "slideDown", duration: 200 }}
+					leave={{ animation: "slideUp", duration: 200 }}
+				>
+					{this.state.showContent ? (
+						<ToggleContent
+							dangerouslySetInnerHTML={{ __html: this.props.content }}
+						/>
+					) : null}
+				</VelocityTransitionGroup>
+			</Toggle>
+		);
+	}
 }
 
 export default Accordion;
--- src/components/layouts/TogglesLayout.js
+++ src/components/layouts/TogglesLayout.js
@@ -1,29 +1,37 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import Accordion from "../Accordion";
 
+const Toggles = styled.section`
+	margin-bottom: 2rem;
+`;
+
+const Header = styled.h2`
+	text-align: center;
+`;
+
+const Blurb = styled.div`
+	text-align: center;
+`;
+
 const TogglesLayout = props => (
-  <section className="toggles">
-    {props.layout.header ? (
-      <h2
-        className="layout-header"
-        dangerouslySetInnerHTML={{ __html: props.layout.header }}
-      />
-    ) : (
-      ""
-    )}
-    {props.layout.blurb ? (
-      <div dangerouslySetInnerHTML={{ __html: props.layout.blurb }} />
-    ) : (
-      ""
-    )}
-    {props.layout.toggles.map((toggle, index) => (
-      <Accordion
-        key={`toggle-${index}`}
-        header={toggle.toggle_header}
-        content={toggle.toggle_content}
-      />
-    ))}
-  </section>
+	<Toggles>
+		{props.layout.header ? (
+			<Header dangerouslySetInnerHTML={{ __html: props.layout.header }} />
+		) : null}
+
+		{props.layout.blurb ? (
+			<Blurb dangerouslySetInnerHTML={{ __html: props.layout.blurb }} />
+		) : null}
+
+		{props.layout.toggles.map((toggle, index) => (
+			<Accordion
+				key={`toggle-${index}`}
+				header={toggle.toggle_header}
+				content={toggle.toggle_content}
+			/>
+		))}
+	</Toggles>
 );
 
 export default TogglesLayout;
