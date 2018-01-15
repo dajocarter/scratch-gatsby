0796714d7b1b07e5b55ad6ee03bbe077eb2a33cc
David Carter <dajocarter@gmail.com>
2018-01-15 00:09:37 -0500

    Convert hero unit layout styled-components

--- src/components/layouts/HeroLayout.js
+++ src/components/layouts/HeroLayout.js
@@ -1,45 +1,98 @@
 import React, { Component } from "react";
 import Link from "gatsby-link";
+import styled from "styled-components";
+import { darken, clearFix } from "polished";
 import "./HeroLayout.scss";
+
+const HeroUnit = styled.section`
+	width: 100vw;
+	position: relative;
+	left: 50%;
+	right: 50%;
+	margin-left: -50vw;
+	margin-right: -50vw;
+`;
+
+const BackgroundImage = styled.div`
+	position: relative;
+	background-image: ${props => `url(${props.url})`};
+	background-position: ${props => props.position};
+	background-repeat: no-repeat;
+	background-size: cover;
+`;
+
+const Overlay = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background: rgba(0, 0, 0, 0.25);
+`;
+
+const Wrap = styled.div`
+	${clearFix()};
+	margin: 0 auto;
+	max-width: 1024px;
+	padding: 0 1rem;
+`;
+
+const Content = styled.div`
+	position: relative;
+	color: #fff;
+	text-align: ${props => props.textAlign};
+	padding: ${props => `${props.textMargin}rem 0`};
+`;
+
+const Header = styled.h2`
+	margin-top: 0;
+`;
+
+const Blurb = styled.div``;
+
+const Button = styled(Link)`
+	background: #3d9970;
+	border-radius: 0.125rem;
+	color: #fff;
+	display: inline-block;
+	margin-top: 1rem;
+	padding: 1rem;
+	text-decoration: none;
+
+	&:hover {
+		background: ${darken(0.05, `#3d9970`)};
+	}
+`;
 
 class HeroLayout extends Component {
 	render() {
 		const layout = this.props.layout;
 		return (
-			<section className={`hero-unit`}>
-				<div
-					className="scratch-bg"
-					style={{
-						backgroundImage: `url(${
-							layout.background_image.localFile.childImageSharp.original.src
-						})`,
-						backgroundPosition: `${layout.bg_image_pos_x} ${
-							layout.bg_image_pos_y
-						}`
-					}}
+			<HeroUnit>
+				<BackgroundImage
+					url={layout.background_image.localFile.childImageSharp.original.src}
+					position={`${layout.bg_image_pos_x} ${layout.bg_image_pos_y}`}
 				>
-					<div className="overlay" />
-					<div className="wrap">
-						<div
-							className={`content ${layout.text_align}`}
-							style={{ padding: `${layout.text_margin}rem 0` }}
+					<Overlay />
+					<Wrap>
+						<Content
+							textAlign={layout.text_align}
+							textMargin={layout.text_margin}
 						>
-							<h2 className="layout-header">{layout.header}</h2>
-							<div dangerouslySetInnerHTML={{ __html: layout.blurb }} />
+							<Header>{layout.header}</Header>
+							<Blurb dangerouslySetInnerHTML={{ __html: layout.blurb }} />
 							{layout.add_button ? (
-								<p className="button-wrapper">
-									<Link
-										to={layout.button_link.url}
-										target={layout.button_link.target}
-									>
-										{layout.button_link.title}
-									</Link>
-								</p>
+								<Button
+									to={layout.button_link.url}
+									target={layout.button_link.target}
+								>
+									{layout.button_link.title}
+								</Button>
 							) : null}
-						</div>
-					</div>
-				</div>
-			</section>
+						</Content>
+					</Wrap>
+				</BackgroundImage>
+			</HeroUnit>
 		);
 	}
 }
