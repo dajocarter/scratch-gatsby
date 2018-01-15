8d20289ddd6596e8ccc9cd3297003c42d3a546c7
David Carter <dajocarter@gmail.com>
2018-01-15 02:55:16 -0500

    Convert staggered rows layout to styled-components

--- src/components/layouts/StaggeredRowsLayout.js
+++ src/components/layouts/StaggeredRowsLayout.js
@@ -1,71 +1,136 @@
 import React, { Component } from "react";
-import Link from "gatsby-link";
+import styled from "styled-components";
 import "./StaggeredRowsLayout.scss";
+import { darken } from "polished";
+import Link from "gatsby-link";
+
+const StaggeredRows = styled.section``;
+
+const Row = styled.div`
+	width: 100vw;
+	position: relative;
+	left: 50%;
+	right: 50%;
+	margin-left: -50vw;
+	margin-right: -50vw;
+	background-color: ${props => (props.order % 2 ? `#fff` : `#ccc`)};
+`;
+
+const Wrap = styled.div`
+	margin: 0 auto;
+	max-width: 1024px;
+	padding: 3rem 1rem;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
+	align-items: center;
+
+	@media (min-width: 768px) {
+		justify-content: space-between;
+	}
+`;
+
+const ImageContainer = styled.div`
+	flex: 0 0 100%;
+	padding-bottom: 3rem;
+	@media (min-width: 768px) {
+		flex: 0 0 250px;
+		order: ${props => (props.order % 2 ? 1 : 2)};
+		padding-bottom: 0;
+	}
+`;
+
+const Circle = styled.div`
+	height: 250px;
+	width: 250px;
+	border-radius: 50%;
+	margin: 0 auto;
+	background-image: ${props => `url(${props.url})`};
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+`;
+
+const ContentContainer = styled.div`
+	@media (max-width: 767px) {
+		flex: 0 0 100%;
+		text-align: center;
+	}
+	@media (min-width: 768px) {
+		flex: 0 0 60%;
+		order: ${props => (props.order % 2 ? 2 : 1)};
+		text-align: ${props => (props.order % 2 ? `right` : `left`)};
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+`;
+
+const Content = styled.div`
+	@media (min-width: 768px) {
+		flex: 0 0 auto;
+	}
+`;
+
+const Header = styled.h2`
+	margin-top: 0;
+`;
+
+const Blurb = styled.div`
+	p {
+		&:first-child {
+			margin-top: 0;
+		}
+
+		&:last-child {
+			margin-bottom: 0;
+		}
+	}
+`;
+
+const Button = styled(Link)`
+	background: #3d9970;
+	border-radius: 0.125rem;
+	color: #fff;
+	display: inline-block;
+	margin-top: 1rem;
+	padding: 1rem;
+	text-decoration: none;
+
+	&:hover {
+		background: ${darken(0.05, `#3d9970`)};
+	}
+`;
 
 class StaggeredRowsLayout extends Component {
 	render() {
 		const layout = this.props.layout;
 		return (
-			<section className="staggered">
+			<StaggeredRows>
 				{layout.rows.map((row, index) => (
-					<div key={`row-${index}`} className="row">
-						<div className="wrap">
-							<div className="fivecol first">
-								{row.add_button ? (
-									<Link
-										to={row.button_link.url}
-										target={row.button_link.target}
-									>
-										<div
-											className="circle scratch-bg"
-											style={{
-												backgroundImage: `url(${
-													row.image.localFile.childImageSharp.resize.src
-												})`
-											}}
-										/>
-									</Link>
-								) : (
-									<div
-										className="circle scratch-bg"
-										style={{
-											backgroundImage: `url(${
-												row.image.localFile.childImageSharp.resize.src
-											})`
-										}}
-									/>
-								)}
-							</div>
-							<div className="sevencol last">
-								<div className="content valign">
+					<Row key={`row-${index}`} order={index}>
+						<Wrap>
+							<ImageContainer order={index}>
+								<Circle url={row.image.localFile.childImageSharp.resize.src} />
+							</ImageContainer>
+							<ContentContainer order={index}>
+								<Content>
+									<Header>{row.header}</Header>
+									<Blurb dangerouslySetInnerHTML={{ __html: row.blurb }} />
 									{row.add_button ? (
-										<Link
-											to={row.button_link.url}
-											target={row.button_link.target}
-										>
-											{row.header}
-										</Link>
-									) : (
-										<h3>{row.header}</h3>
-									)}
-									<div
-										className="blurb"
-										dangerouslySetInnerHTML={{ __html: row.blurb }}
-									/>
-									{row.add_button ? (
-										<Link
+										<Button
 											to={row.button_link.url}
 											target={row.button_link.target}
 										>
 											{row.button_link.title}
-										</Link>
+										</Button>
 									) : null}
-								</div>
-							</div>
-						</div>
-					</div>
+								</Content>
+							</ContentContainer>
+						</Wrap>
+					</Row>
 				))}
-			</section>
+			</StaggeredRows>
 		);
 	}
 }
