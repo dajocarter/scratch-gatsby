fb3b3a75f54e7108efdfb712fb57004f7b60e887
David Carter <dajocarter@gmail.com>
2018-01-17 00:52:12 -0500

    Refactor Cards layout
    import common styles

--- src/components/layouts/CardsLayout.js
+++ src/components/layouts/CardsLayout.js
@@ -1,93 +1,94 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
-import { darken } from "polished";
-import Link from "gatsby-link";
 import Img from "gatsby-image";
+import { LayoutSection, Wrap, Button } from "../Styles";
 
-const Cards = styled.section`
-	margin: 0 auto 2rem;
-	max-width: 1024px;
-`;
-
-const Wrap = styled.div`
-	display: flex;
-	flex-flow: row wrap;
-	justify-content: space-between;
-	align-content: center;
+const CardsContainer = Wrap.extend`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-content: center;
+  border-radius: 0.25rem;
 `;
 
 const Card = styled.div`
-	flex: 0 0 22.5%;
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	background-color: white;
-	border: 1px solid rgba(0, 0, 0, 0.125);
-	border-radius: 0.25rem;
+  background-color: white;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  box-sizing: border-box;
+  margin-bottom: 2rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex: 0 0 100%;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    flex: 0 0 45%;
+    &:nth-child(even) {
+      margin-left: 10%;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    flex: 0 0 30%;
+    &:nth-child(3n + 1) {
+      margin-left: 0;
+    }
+    &:nth-child(3n),
+    &:nth-child(3n + 2) {
+      margin-left: 5%;
+    }
+  }
 `;
 
 const CardImg = styled(Img)`
-	border-top-right-radius: calc(0.25rem - 1px);
-	border-top-left-radius: calc(0.25rem - 1px);
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
-	width: 100%;
+  border-top-right-radius: calc(0.25rem - 1px);
+  border-top-left-radius: calc(0.25rem - 1px);
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
 `;
 
 const CardBlock = styled.div`
-	flex: 1 1 auto;
-	padding: 1rem;
+  flex: 1 1 auto;
+  padding: 1rem;
 `;
 
 const CardTitle = styled.h4`
-	margin-bottom: 0.75rem;
+  margin-bottom: 0.75rem;
 `;
 
 const CardText = styled.div``;
 
 const Blurb = styled.div``;
 
-const Button = styled(Link)`
-	background: #3d9970;
-	border-radius: 0.125rem;
-	color: #fff;
-	display: inline-block;
-	margin-top: 1rem;
-	padding: 1rem;
-	text-decoration: none;
-
-	&:hover {
-		background: ${darken(0.05, `#3d9970`)};
-	}
-`;
-
 const CardsLayout = props => (
-	<Cards>
-		<Wrap>
-			{props.layout.cards.map((card, index) => (
-				<Card key={`card-${index}`}>
-					{card.image ? (
-						<CardImg sizes={card.image.localFile.childImageSharp.sizes} />
-					) : null}
-					<CardBlock>
-						<CardTitle>{card.header}</CardTitle>
-						<CardText>
-							<Blurb dangerouslySetInnerHTML={{ __html: card.blurb }} />
-							{card.add_button ? (
-								<Button
-									to={card.button_link.url}
-									target={card.button_link.target}
-								>
-									{card.button_link.title}
-								</Button>
-							) : null}
-						</CardText>
-					</CardBlock>
-				</Card>
-			))}
-		</Wrap>
-	</Cards>
+  <LayoutSection>
+    <CardsContainer>
+      {props.layout.cards.map((card, index) => (
+        <Card key={`card-${index}`}>
+          {card.image ? (
+            <CardImg sizes={card.image.localFile.childImageSharp.sizes} />
+          ) : null}
+          <CardBlock>
+            <CardTitle>{card.header}</CardTitle>
+            <CardText>
+              <Blurb dangerouslySetInnerHTML={{ __html: card.blurb }} />
+              {card.add_button ? (
+                <Button
+                  to={card.button_link.url}
+                  target={card.button_link.target}
+                >
+                  {card.button_link.title}
+                </Button>
+              ) : null}
+            </CardText>
+          </CardBlock>
+        </Card>
+      ))}
+    </CardsContainer>
+  </LayoutSection>
 );
 
 export default CardsLayout;
