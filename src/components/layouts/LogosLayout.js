7a9c7d8ed26a139aac5598c8ebdc15e10b11abe1
David Carter <dajocarter@gmail.com>
2018-01-17 01:17:08 -0500

    Refactor Logos layout

--- src/components/layouts/LogosLayout.js
+++ src/components/layouts/LogosLayout.js
@@ -1,26 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import { LayoutSection, Wrap, LayoutHeader } from "../Styles";
 import LogoDisplay from "../LogoDisplay";
 import LogoSlick from "../LogoSlick";
 
-const Logos = styled.section`
-	margin: 0 auto 2rem;
-	max-width: 1024px;
-`;
-
-const Header = styled.h2`
-	text-align: center;
-`;
-
 const LogosLayout = props => (
-	<Logos>
-		<Header>{props.layout.header}</Header>
-		{props.layout.logo_display === "all" ? (
-			<LogoDisplay logos={props.layout.logos} />
-		) : (
-			<LogoSlick logos={props.layout.logos} />
-		)}
-	</Logos>
+  <LayoutSection>
+    <Wrap>
+      <LayoutHeader>{props.layout.header}</LayoutHeader>
+      {props.layout.logo_display === "all" ? (
+        <LogoDisplay logos={props.layout.logos} />
+      ) : (
+        <LogoSlick logos={props.layout.logos} />
+      )}
+    </Wrap>
+  </LayoutSection>
 );
 
 export default LogosLayout;
