edbb65f22b455e5befab03cc2bfc82f2b8f69ebd
David Carter <dajocarter@gmail.com>
2018-01-17 01:51:10 -0500

    Refactor staggered rows
    import common styles and convert to stateless

--- src/components/layouts/StaggeredRowsLayout.js
+++ src/components/layouts/StaggeredRowsLayout.js
@@ -1,26 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
-import { darken } from "polished";
-import Link from "gatsby-link";
-
-const StaggeredRows = styled.section``;
+import {
+  LayoutSection,
+  FlexWrap,
+  Circle,
+  LayoutHeader,
+  LayoutBlurb,
+  Button
+} from "../Styles";
 
 const Row = styled.div`
-  background-color: ${props => (props.order % 2 ? `#fff` : `#ccc`)};
-`;
-
-const Wrap = styled.div`
-  margin: 0 auto;
-  max-width: 1024px;
+  background-color: ${props => (props.rowIndex % 2 ? `#fff` : `#ccc`)};
   padding: 3rem 1rem;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
-
-  @media (min-width: 768px) {
-    justify-content: space-between;
-  }
 `;
 
 const ImageContainer = styled.div`
@@ -28,20 +19,14 @@ const ImageContainer = styled.div`
   padding-bottom: 3rem;
   @media (min-width: 768px) {
     flex: 0 0 250px;
-    order: ${props => (props.order % 2 ? 1 : 2)};
+    order: ${props => (props.rowIndex % 2 ? 1 : 2)};
     padding-bottom: 0;
   }
 `;
 
-const Circle = styled.div`
+const Image = Circle.extend`
   height: 250px;
   width: 250px;
-  border-radius: 50%;
-  margin: 0 auto;
-  background-image: ${props => `url(${props.url})`};
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
 `;
 
 const ContentContainer = styled.div`
@@ -51,77 +36,57 @@ const ContentContainer = styled.div`
   }
   @media (min-width: 768px) {
     flex: 0 0 60%;
-    order: ${props => (props.order % 2 ? 2 : 1)};
-    text-align: ${props => (props.order % 2 ? `right` : `left`)};
+    order: ${props => (props.rowIndex % 2 ? 2 : 1)};
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: ${props => (props.rowIndex % 2 ? `right` : `left`)};
   }
 `;
 
 const Content = styled.div``;
 
-const Header = styled.h2`
-  margin-top: 0;
-`;
-
-const Blurb = styled.div`
-  p {
-    &:first-child {
-      margin-top: 0;
-    }
-
-    &:last-child {
-      margin-bottom: 0;
-    }
+const Header = LayoutHeader.extend`
+  @media (min-width: 768px) {
+    text-align: ${props => (props.rowIndex % 2 ? `right` : `left`)};
   }
 `;
 
-const Button = styled(Link)`
-  background: #3d9970;
-  border-radius: 0.125rem;
-  color: #fff;
-  display: inline-block;
-  margin-top: 1rem;
-  padding: 1rem;
-  text-decoration: none;
-
-  &:hover {
-    background: ${darken(0.05, `#3d9970`)};
+const Blurb = LayoutBlurb.extend`
+  @media (min-width: 768px) {
+    text-align: ${props => (props.rowIndex % 2 ? `right` : `left`)};
   }
 `;
 
-class StaggeredRowsLayout extends Component {
-  render() {
-    const layout = this.props.layout;
-    return (
-      <StaggeredRows>
-        {layout.rows.map((row, index) => (
-          <Row key={`row-${index}`} order={index}>
-            <Wrap>
-              <ImageContainer order={index}>
-                <Circle url={row.image.localFile.childImageSharp.resize.src} />
-              </ImageContainer>
-              <ContentContainer order={index}>
-                <Content>
-                  <Header>{row.header}</Header>
-                  <Blurb dangerouslySetInnerHTML={{ __html: row.blurb }} />
-                  {row.add_button ? (
-                    <Button
-                      to={row.button_link.url}
-                      target={row.button_link.target}
-                    >
-                      {row.button_link.title}
-                    </Button>
-                  ) : null}
-                </Content>
-              </ContentContainer>
-            </Wrap>
-          </Row>
-        ))}
-      </StaggeredRows>
-    );
-  }
-}
+const StaggeredRowsLayout = props => (
+  <LayoutSection>
+    {props.layout.rows.map((row, index) => (
+      <Row key={`row-${index}`} rowIndex={index}>
+        <FlexWrap>
+          <ImageContainer rowIndex={index}>
+            <Image url={row.image.localFile.childImageSharp.resize.src} />
+          </ImageContainer>
+          <ContentContainer rowIndex={index}>
+            <Content>
+              <Header rowIndex={index}>{row.header}</Header>
+              <Blurb
+                rowIndex={index}
+                dangerouslySetInnerHTML={{ __html: row.blurb }}
+              />
+              {row.add_button ? (
+                <Button
+                  to={row.button_link.url}
+                  target={row.button_link.target}
+                >
+                  {row.button_link.title}
+                </Button>
+              ) : null}
+            </Content>
+          </ContentContainer>
+        </FlexWrap>
+      </Row>
+    ))}
+  </LayoutSection>
+);
 
 export default StaggeredRowsLayout;
