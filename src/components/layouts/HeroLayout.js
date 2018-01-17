e5e091f1701e1672de992ba6ea099345bba8714f
David Carter <dajocarter@gmail.com>
2018-01-17 00:35:24 -0500

    Refactor Hero Unit layout
    import comps from Styles and convert to stateless

--- src/components/layouts/HeroLayout.js
+++ src/components/layouts/HeroLayout.js
@@ -1,9 +1,13 @@
-import React, { Component } from "react";
-import Link from "gatsby-link";
+import React from "react";
 import styled from "styled-components";
-import { darken, clearFix } from "polished";
-
-const HeroUnit = styled.section``;
+import {
+  LayoutSection,
+  Overlay,
+  Wrap,
+  LayoutHeader,
+  LayoutBlurb,
+  Button
+} from "../Styles";
 
 const BackgroundImage = styled.div`
   position: relative;
@@ -13,22 +17,6 @@ const BackgroundImage = styled.div`
   background-size: cover;
 `;
 
-const Overlay = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.25);
-`;
-
-const Wrap = styled.div`
-  ${clearFix()};
-  margin: 0 auto;
-  max-width: 1024px;
-  padding: 0 1rem;
-`;
-
 const Content = styled.div`
   position: relative;
   color: #fff;
@@ -36,67 +24,34 @@ const Content = styled.div`
   padding: ${props => `${props.textMargin}rem 0`};
 `;
 
-const Header = styled.h2`
-  margin-top: 0;
-`;
-
-const Blurb = styled.div`
-  p {
-    &:first-child {
-      margin-top: 0;
-    }
-
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-`;
-
-const Button = styled(Link)`
-  background: #3d9970;
-  border-radius: 0.125rem;
-  color: #fff;
-  display: inline-block;
-  margin-top: 1rem;
-  padding: 1rem;
-  text-decoration: none;
-
-  &:hover {
-    background: ${darken(0.05, `#3d9970`)};
-  }
-`;
-
-class HeroLayout extends Component {
-  render() {
-    const layout = this.props.layout;
-    return (
-      <HeroUnit>
-        <BackgroundImage
-          url={layout.background_image.localFile.childImageSharp.original.src}
-          position={`${layout.bg_image_pos_x} ${layout.bg_image_pos_y}`}
+const HeroLayout = props => (
+  <LayoutSection>
+    <BackgroundImage
+      url={props.layout.background_image.localFile.childImageSharp.original.src}
+      position={`${props.layout.bg_image_pos_x} ${props.layout.bg_image_pos_y}`}
+    >
+      <Overlay />
+      <Wrap>
+        <Content
+          textAlign={props.layout.text_align}
+          textMargin={props.layout.text_margin}
         >
-          <Overlay />
-          <Wrap>
-            <Content
-              textAlign={layout.text_align}
-              textMargin={layout.text_margin}
+          <LayoutHeader>{props.layout.header}</LayoutHeader>
+          <LayoutBlurb
+            dangerouslySetInnerHTML={{ __html: props.layout.blurb }}
+          />
+          {props.layout.add_button ? (
+            <Button
+              to={props.layout.button_link.url}
+              target={props.layout.button_link.target}
             >
-              <Header>{layout.header}</Header>
-              <Blurb dangerouslySetInnerHTML={{ __html: layout.blurb }} />
-              {layout.add_button ? (
-                <Button
-                  to={layout.button_link.url}
-                  target={layout.button_link.target}
-                >
-                  {layout.button_link.title}
-                </Button>
-              ) : null}
-            </Content>
-          </Wrap>
-        </BackgroundImage>
-      </HeroUnit>
-    );
-  }
-}
+              {props.layout.button_link.title}
+            </Button>
+          ) : null}
+        </Content>
+      </Wrap>
+    </BackgroundImage>
+  </LayoutSection>
+);
 
 export default HeroLayout;
