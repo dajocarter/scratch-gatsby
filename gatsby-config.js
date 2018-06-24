75c0f7b03a918ad8746652d34f40361eb75c0256
David Carter <dajocarter@gmail.com>
2018-06-24 13:55:55 -0400

    Editor changes
    Also changed WordPress source URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 require("dotenv").config();
 
 module.exports = {
-	siteMetadata: {
-		title: `Gatsby Default Starter`
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		{
-			resolve: `gatsby-source-wordpress`,
-			options: {
-				baseUrl: `perfect.tribestaging.com`,
-				protocol: `https`,
-				hostingWPCOM: false,
-				useACF: true,
-				verboseOutput: true,
-				auth: {
-					htaccess_user: process.env.HTACCESS_USER,
-					htaccess_pass: process.env.HTACCESS_PASS,
-					htaccess_sendImmediately: false
-				}
-			}
-		},
-		`gatsby-plugin-sass`,
-		`gatsby-plugin-styled-components`,
-		{
-			resolve: `gatsby-plugin-google-fonts`,
-			options: {
-				fonts: [`Lato`]
-			}
-		},
-		`gatsby-plugin-sharp`,
-		`gatsby-transformer-sharp`
-	]
+  siteMetadata: {
+    title: `Gatsby Default Starter`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `scratchgatsby.wpengine.com`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: true,
+        verboseOutput: true,
+        auth: {
+          htaccess_user: process.env.HTACCESS_USER,
+          htaccess_pass: process.env.HTACCESS_PASS,
+          htaccess_sendImmediately: false
+        }
+      }
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Lato`]
+      }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`
+  ]
 };
